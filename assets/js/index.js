83049484801d2ba0081c35c1c4175d3f21cc5289
SamueleConverso <samu.converso@gmail.com>
2024-12-13 12:32:42 +0100

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,8 @@ let apiKey =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzViZjcxMWQyMjA3MTAwMTVkZTJmM2MiLCJpYXQiOjE3MzQwODAyNzQsImV4cCI6MTczNTI4OTg3NH0.v17yR1ttMjJ502S2x6eTRuGLyGMxouajUcqejbw_Pes";
 let data;
 
+const mainRow = document.getElementById("mainRow");
+
 async function getData() {
   try {
     let response = await fetch(endPoint, {
@@ -12,10 +14,50 @@ async function getData() {
     });
     data = await response.json();
     console.log(data);
-    //printImage();
+    printData();
   } catch (error) {
     console.log(error);
   }
 }
 
-getData();
+document.addEventListener("load", init());
+
+function init() {
+  getData();
+}
+
+function printData() {
+  for (let i = 0; i < data.length; i++) {
+    let cardDiv = document.createElement("div");
+    cardDiv.classList.add("col-3");
+    cardDiv.innerHTML = `
+    <div class="card" style="width: 18rem;">
+  <img src="${data[i].imageUrl}" class="card-img-top prodImg" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${data[i].name}</h5>
+    <p class="card-text">${data[i].description}</p>
+    <p class="card-text h6">${data[i].price}€</p>
+    <button type="button" class="btn btn-primary btnModify">Modify</button>
+    <button type="button" class="btn btn-warning btnDetail">Detail</button>
+  </div>
+</div>
+    `;
+    mainRow.appendChild(cardDiv);
+
+    const btnModify = cardDiv.querySelector(".btnModify");
+    btnModify.addEventListener("click", (e) => {
+      e.preventDefault();
+      let secondPage = "office.html";
+      let newUrl = `${secondPage}?_id=${data[i]._id}`;
+      window.location.href = newUrl;
+    });
+
+    const btnDetail = cardDiv.querySelector(".btnDetail");
+    btnDetail.addEventListener("click", (e) => {
+      e.preventDefault();
+      let secondPage = "detail.html";
+      let newUrl = `${secondPage}?_id=${data[i]._id}`;
+      window.location.href = newUrl;
+    });
+  }
+}
---
+++ assets/js/office.js
@@ -0,0 +1,134 @@
+let endPoint = "https://striveschool-api.herokuapp.com/api/product/";
+let apiKey =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzViZjcxMWQyMjA3MTAwMTVkZTJmM2MiLCJpYXQiOjE3MzQwODAyNzQsImV4cCI6MTczNTI4OTg3NH0.v17yR1ttMjJ502S2x6eTRuGLyGMxouajUcqejbw_Pes";
+
+const url = new URLSearchParams(window.location.search);
+const modifyProductId = url.get("_id");
+
+const inputProductName = document.getElementById("inputProductName");
+const inputProductDesc = document.getElementById("inputProductDesc");
+const inputProductBrand = document.getElementById("inputProductBrand");
+const inputProductImageUrl = document.getElementById("inputProductImageUrl");
+const inputProductPrice = document.getElementById("inputProductPrice");
+
+const btnDelete = document.getElementById("btnDelete");
+const btnCancel = document.getElementById("btnCancel");
+const btnAdd = document.getElementById("btnAdd");
+
+let data;
+let product;
+let productId = null || modifyProductId;
+
+document.addEventListener("load", init());
+
+function init() {
+  if (productId) {
+    getData();
+  }
+}
+
+async function getData() {
+  try {
+    let response = await fetch(endPoint + productId, {
+      headers: {
+        Authorization: apiKey,
+      },
+    });
+    data = await response.json();
+    console.log(data);
+    printData();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function printData() {
+  inputProductName.value = data.name;
+  inputProductDesc.value = data.description;
+  inputProductBrand.value = data.brand;
+  inputProductImageUrl.value = data.imageUrl;
+  inputProductPrice.value = data.price;
+}
+
+class Product {
+  constructor(
+    _productName,
+    _productDesc,
+    _productBrand,
+    _productImageUrl,
+    _productPrice
+  ) {
+    this.name = _productName;
+    this.description = _productDesc;
+    this.brand = _productBrand;
+    this.imageUrl = _productImageUrl;
+    this.price = _productPrice;
+  }
+}
+
+btnAdd.addEventListener("click", (e) => {
+  e.preventDefault;
+  let prodName = inputProductName.value;
+  let prodDesc = inputProductDesc.value;
+  let prodBrand = inputProductBrand.value;
+  let prodImageUrl = inputProductImageUrl.value;
+  let prodPrice = inputProductPrice.value;
+  product = new Product(prodName, prodDesc, prodBrand, prodImageUrl, prodPrice);
+  console.log(product);
+  if (!productId) {
+    addData();
+  } else {
+    modifyData();
+  }
+});
+
+async function addData() {
+  try {
+    let response = await fetch(endPoint, {
+      method: "POST",
+      body: JSON.stringify(product),
+      headers: {
+        Authorization: apiKey,
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function modifyData() {
+  try {
+    let response = await fetch(endPoint + productId, {
+      method: "PUT",
+      body: JSON.stringify(product),
+      headers: {
+        Authorization: apiKey,
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+btnDelete.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (productId) {
+    deleteData();
+  }
+  console.log("Cancellato");
+});
+
+async function deleteData() {
+  try {
+    let response = await fetch(endPoint + productId, {
+      method: "DELETE",
+      headers: {
+        Authorization: apiKey,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
